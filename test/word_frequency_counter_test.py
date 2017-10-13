e1476d52648844371fa96a56295f79b9362ab463
Dario Coco <dario@igenius.net>
2017-10-13 10:56:22 +0200

    Rewrite first test

--- test/word_frequency_counter_test.py
+++ test/word_frequency_counter_test.py
@@ -4,4 +4,5 @@ class TestWordFrequencyCounter(TestCase):
 
 	def empty_string_should_return_0_test(self):
 		frequencyCounter = WordFrequencyCounter()
+		frequencyCounter.load_from_string('a b c d')
 		self.assertEqual(0, frequencyCounter.count(''))
