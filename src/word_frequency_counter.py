62be13bba5d7deca8c949fc1ffff75067cd3cfe2
Dario Coco <dario@igenius.net>
2017-10-13 11:57:06 +0200

    Removed unnecessary code and its tests

--- src/word_frequency_counter.py
+++ src/word_frequency_counter.py
@@ -4,9 +4,5 @@ class WordFrequencyCounter:
 	def load_from_string(self, words):
 		self.words = words.split(' ')
 
-	def count(self, word):
-		return self.words.count(word)
-
 	def summary(self):
 		return dict(Counter(self.words))
-
--- test/word_frequency_counter_test.py
+++ test/word_frequency_counter_test.py
@@ -3,26 +3,8 @@ from src.word_frequency_counter import WordFrequencyCounter
 
 class TestWordFrequencyCounter(TestCase):
 
-	simple_test_string = 'a b c d'
-
 	def setUp(self):
 		self.frequencyCounter = WordFrequencyCounter()
-
-	def empty_string_should_return_0_test(self):
-		self.frequencyCounter.load_from_string('a b c d')
-		self.assertEqual(0, self.frequencyCounter.count(''))
-
-	def finds_one_occurrence_test(self):
-		self.frequencyCounter.load_from_string('a b c d')
-		self.assertEqual(1, self.frequencyCounter.count('a'))
-
-	def finds_zero_occurrences_if_word_not_found_test(self):
-		self.frequencyCounter.load_from_string('a b c d')
-		self.assertEqual(0, self.frequencyCounter.count('e'))
-
-	def finds_two_occurrences_test(self):
-		self.frequencyCounter.load_from_string('a a b c d')
-		self.assertEqual(2, self.frequencyCounter.count('a'))
 
 	def summary_should_return_all_frequencies_test(self):
 		self.frequencyCounter.load_from_string('a a a a b b b c c d')
