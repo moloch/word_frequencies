936b0950b20239ec8427b7d685901858915a0c32
Dario Coco <dario@igenius.net>
2017-10-13 11:21:06 +0200

    Added 0 matches test

--- test/word_frequency_counter_test.py
+++ test/word_frequency_counter_test.py
@@ -3,12 +3,19 @@ from src.word_frequency_counter import WordFrequencyCounter
 
 class TestWordFrequencyCounter(TestCase):
 
+	simple_test_string = 'a b c d'
+
+	def setUp(self):
+		self.frequencyCounter = WordFrequencyCounter()
+
 	def empty_string_should_return_0_test(self):
-		frequencyCounter = WordFrequencyCounter()
-		frequencyCounter.load_from_string('a b c d')
-		self.assertEqual(0, frequencyCounter.count(''))
+		self.frequencyCounter.load_from_string('a b c d')
+		self.assertEqual(0, self.frequencyCounter.count(''))
 
 	def finds_one_occurrence_test(self):
-		frequencyCounter = WordFrequencyCounter()
-		frequencyCounter.load_from_string('a b c d')
-		self.assertEqual(1, frequencyCounter.count('a'))
+		self.frequencyCounter.load_from_string('a b c d')
+		self.assertEqual(1, self.frequencyCounter.count('a'))
+
+	def finds_zero_occurrences_if_word_not_found_test(self):
+		self.frequencyCounter.load_from_string('a b c d')
+		self.assertEqual(0, self.frequencyCounter.count('e'))
