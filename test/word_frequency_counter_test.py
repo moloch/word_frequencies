4d39ccb2e1559fd04e338862432975523afe58e7
Dario Coco <dario@igenius.net>
2017-10-13 11:40:27 +0200

    Added failing test for WordFrequencyCounter.summary

--- test/word_frequency_counter_test.py
+++ test/word_frequency_counter_test.py
@@ -23,3 +23,11 @@ class TestWordFrequencyCounter(TestCase):
 	def finds_two_occurrences_test(self):
 		self.frequencyCounter.load_from_string('a a b c d')
 		self.assertEqual(2, self.frequencyCounter.count('a'))
+
+	def summary_should_return_all_frequencies_test(self):
+		self.frequencyCounter.load_from_string('a a a a b b b c c d')
+		summary = self.frequencyCounter.summary()
+		self.assertEqual(4, summary['a'])
+		self.assertEqual(3, summary['b'])
+		self.assertEqual(2, summary['c'])
+		self.assertEqual(1, summary['d'])
