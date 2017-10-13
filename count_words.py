816875f2d42e92f6bdebfbb6a0b8d45abb18e21b
Dario Coco <dario@igenius.net>
2017-10-13 16:39:39 +0200

    Fixed count_words script after refactoring.

--- count_words.py
+++ count_words.py
@@ -1,9 +1,9 @@
 import sys
 from src.word_frequency_counter import WordFrequencyCounter
+from src.loaders import FileLoader
 
 def count_words():
-	wfc = WordFrequencyCounter()
-	wfc.load_from_file(sys.argv[1])
+	wfc = WordFrequencyCounter(FileLoader().load(sys.argv[1]))
 	header = 'Word frequencies for ' + sys.argv[1] + ':\n'
 	print(header + wfc.textual_summary())
 
