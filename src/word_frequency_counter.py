d4d6ca7afb17dcfb9c940a60cb23014376e99e59
Dario Coco <dario@igenius.net>
2017-10-13 11:32:26 +0200

    Replaced unnecessary code with pythonic code

--- src/word_frequency_counter.py
+++ src/word_frequency_counter.py
@@ -1,9 +1,6 @@
 class WordFrequencyCounter:
 	def load_from_string(self, words):
-		self.words = words
+		self.words = words.split(' ')
 
 	def count(self, word):
-		if word in self.words.split():
-			return 1
-		else:
-			return 0
+		return self.words.count(word)
