c0acfd110b7675814287c0c4c8ff9fce8604387f
Dario Coco <dario@igenius.net>
2017-10-19 12:21:48 +0200

    Added setup.py

--- count_words.py
+++ count_words.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 from src.word_frequency_counter import WordFrequencyCounter
 from src.loaders import FileLoader
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name = "CountWords",
+    version = "0.1",
+    packages = find_packages(),
+    scripts = ['count_words.py'] 
+)
